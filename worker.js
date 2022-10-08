770613ad69f9a597a5b96be326fecf8f7c3168b1
Matt Butterfield <matt@mattbutterfield.com>
2022-10-08 13:32:41 -0400

    make POST work

--- worker.js
+++ worker.js
@@ -14,9 +14,9 @@ importScripts("./node_modules/xhr-shim/src/index.js");
 self.XMLHttpRequest = self.XMLHttpRequestShim;
 
 let pyodide, loaded;
+const cookies = {};
 
-const setupPyodide = async () => {
-  console.log("loading pyodide.");
+const setupPython = async () => {
   pyodide = await loadPyodide();
   await pyodide.loadPackage("micropip");
   const micropip = pyodide.pyimport("micropip");
@@ -27,9 +27,57 @@ const setupPyodide = async () => {
 };
 
 self.addEventListener("install", event => {
-  console.log("installing...");
-  event.waitUntil(setupPyodide());
+  event.waitUntil(setupPython());
 });
+
+const djangoRequest = async request => {
+  let cookie = "";
+  for (const key in cookies) {
+    const val = `${key}=${cookies[key]}`;
+    cookie += cookie ? `; ${val}` : val;
+  }
+  const reqHeaders = {
+    ...request.headers,
+    Cookie: cookie,
+  };
+  console.log(reqHeaders);
+
+  const method = request.method.toLowerCase();
+  let params = "";
+  if (method === "post") {
+    params = await request.text();
+  }
+
+  const response = pyodide.runPython(`
+      response = app.${method}(
+          "${request.url}",
+          params="${params}",
+          headers=${JSON.stringify(reqHeaders)},
+          expect_errors=True,
+      )
+      try:
+          body = response.text
+      except UnicodeDecodeError:
+          body = response.body
+      body`
+  );
+  const headersPy = pyodide.runPython(`
+      import json
+      json.dumps(dict(response.headers))`);
+  const respHeaders = JSON.parse(headersPy);
+  const setCookie = respHeaders["Set-Cookie"];
+  if (setCookie) {
+    // only setting first one right now
+    const nameValue = setCookie.split(";")[0].split("=");
+    cookies[nameValue[0].trim()] = nameValue[1];
+  }
+  if (respHeaders["Content-Type"] === "application/octet-stream" && request.url.endsWith(".woff")) {
+    respHeaders["Content-Type"] = "application/font-woff";
+  }
+  const status = pyodide.runPython("response.status_code");
+
+  return new Response(response, {headers: respHeaders, status: status});
+};
 
 self.addEventListener("fetch", event => {
   if (!loaded) {
@@ -37,29 +85,7 @@ self.addEventListener("fetch", event => {
       Promise.reject("Python code isn't set up yet, try again in a bit"),
     );
   } else {
-    const method = event.request.method.toLowerCase();
-    console.log(event.request.headers);
-    const reqHeaders = event.request.headers;
-    const response = pyodide.runPython(`
-      response = app.${method}("${event.request.url}", expect_errors=True) # , headers=${JSON.stringify(reqHeaders)})
-      try:
-          body = response.text
-      except UnicodeDecodeError:
-          body = response.body
-      body`
-    );
-    const headersPy = pyodide.runPython(`
-      import json
-      json.dumps(dict(response.headers))`);
-    const respHeaders = JSON.parse(headersPy);
-    if (respHeaders["Content-Type"] === "application/octet-stream" && event.request.url.endsWith(".woff")) {
-      respHeaders["Content-Type"] = "application/font-woff";
-    }
-    const status = pyodide.runPython("response.status_code");
-
-    event.respondWith(
-      new Response(response, {headers: respHeaders, status: status})
-    );
+    event.respondWith(djangoRequest(event.request));
   }
 });
 
