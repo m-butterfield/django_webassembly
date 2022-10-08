6ae4627dafcd08d7d30f429d795297d7c3eb5fce
Matt Butterfield <matt@mattbutterfield.com>
2022-10-08 15:15:20 -0400

    support voting and results

--- init.py
+++ init.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 
 import django
 
@@ -29,13 +30,22 @@ MigrateCommand().handle(
 
 from django.contrib.auth.models import User
 
+# add a user for accessing the admin
 user = User(username="matt", is_staff=True, is_superuser=True)
+user.set_password("password")
 user.save()
 
-# hack to bypass auth in admin
-from django_webassembly.polls.admin import admin
+# uncomment to bypass auth in admin
+# from django_webassembly.polls.admin import admin
+#
+# admin.site.has_permission = lambda r: setattr(r, "user", user) or True
 
-admin.site.has_permission = lambda r: setattr(r, "user", user) or True
+# add some data for the polls app
+from django_webassembly.polls.models import Choice, Question
+q1 = Question(question_text="Dogs or cats?", pub_date=datetime.now())
+q1.save()
+Choice(question=q1, choice_text="dogs").save()
+Choice(question=q1, choice_text="cats").save()
 
 # set up the app we'll use to serve requests
 from django.core.wsgi import get_wsgi_application
